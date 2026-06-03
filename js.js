23bc17b1e7fe3af041bfb71529f6af685483d539
samuel71717 <samuelmelosantos918@gmail.com>
2026-06-03 08:28:26 -0300

    Atualizando HTML e CSS

--- js.js
+++ js.js
@@ -1,50 +1,27 @@
-// Scroll effect + sticky header + active link
-
-const header = document.querySelector('header');
-const sections = document.querySelectorAll('section[id]');
-
-// Header sticky
-function handleHeaderSticky() {
-    if (window.scrollY > 30) {
-        header.classList.add('sticky');
-    } else {
-        header.classList.remove('sticky');
-    }
-}
-
-// Active link on scroll
-function setActiveLink() {
-    let scrollY = window.pageYOffset;
-
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight;
-        const sectionTop = current.offsetTop - 100;
-        const sectionId = current.getAttribute('id');
-
-        const navLink = document.querySelector('.nav-items a[href*=' + sectionId + ']');
-
-        if (navLink) {
-            if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-                navLink.classList.add('active');
-            } else {
-                navLink.classList.remove('active');
-            }
-        }
-    });
-}
-
-// Scroll events
-window.addEventListener('scroll', () => {
-    handleHeaderSticky();
-    setActiveLink();
+//Efect scroll 
+window.addEventListener("scroll", function(){
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
 });
 
-// Load events
-window.addEventListener('load', () => {
-    handleHeaderSticky();
-    setActiveLink();
-});
+const serviceModals = document.querySelector(".service-modal");
+const learnmoreBtns = document.querySelector(".learn-more-btn");
+const modalCloseBtns = document.querySelector(".modal-close-btn");
 
-// Teste GitHub
-console.log("JS atualizado com sucesso");
-console.log("TESTE NOVO");
+var modal = function (modalClick) {
+    serviceModals[modalClick].classList.add("active");
+}
+
+learnmoreBtns.forEach((learnmoreBtn, i) => {
+    learnmoreBtn.addEventListener("click", () =>{
+        modal(i);
+    })
+})
+
+modalCloseBtns.forEach((modalCloseBtn) => {
+    modalCloseBtn.addEventListener("click", () => {
+         serviceModals.forEach((modalView) => {
+             modalView.classList.remove("active");
+         })
+    })
+})
